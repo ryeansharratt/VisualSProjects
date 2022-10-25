ee3b551582c3e848af63e0ae0c302b1a09970f5d
ryeansharratt <ryean.ann@gmail.com>
2022-10-25 17:39:17 -0600

    VS HTML Assignment submission

---
+++ Ryean-VSHTMLassignment/Ryean-VSHTMLassignment/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+//functions to be called onmouseover and onmouseout to change the color of the title
+
+function newColor() {
+    document.getElementById("song").style.color = "darkgrey";
+}
+
+function oldColor() {
+    document.getElementById("song").style.color = "black";
+}
